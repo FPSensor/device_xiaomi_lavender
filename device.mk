957fb9afe890d0c47468aca739f9315714852e32
ImPrashantt <prashant33968@gmail.com>
2023-01-20 02:38:34 +0000

    Revert "lavender : Drop in-tree hals"
    This reverts commit 867794b2829b4ed3850419374faa0fb0902902e3.

--- device.mk
+++ device.mk
@@ -44,6 +44,10 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
+# QCOM namepsaces
+QCOM_SOONG_NAMESPACE := \
+    $(LOCAL_PATH)/qcom-caf
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
